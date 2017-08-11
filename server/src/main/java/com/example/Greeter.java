3dd1a9b53b4a5cb3e89b47b481ec57bfe89579d8
Ashwin Patil <ashwin.patil@hotmail.com>
2017-08-11 13:16:08 -0700

    fix plugin warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-  
+
   /**
-  *@param someone
-  *@return greeting
-  **/
-  
+  *@param someone the name of a person
+  *@return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
